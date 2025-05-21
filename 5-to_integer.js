256bd0f83a4940b64898aab0c916e77a59e6168f
Festus Kimani <171272139+kingfetson@users.noreply.github.com>
2025-05-21 20:40:13 +0300

    Create 5-to_integer.js

---
+++ 5-to_integer.js
@@ -0,0 +1,10 @@
+// Script that prints "My number: <int>" if convertible, else "Not a number"
+
+const arg = process.argv[2];
+const num = parseInt(arg);
+
+if (!isNaN(num)) {
+  console.log(`My number: ${num}`);
+} else {
+  console.log('Not a number');
+}
